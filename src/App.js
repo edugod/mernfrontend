84a47c98265be2de9bd06a19ec88d39737245177
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-05-09 20:58:31 -0300

    lets gooo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
-import Login from "./components/Login";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
 
 function App() {
 	return (
@@ -9,9 +13,25 @@ function App() {
 			<Route path="/" element={<Layout />}>
 				<Route index element={<Public />} />
 				<Route path="login" element={<Login />} />
+
+				<Route path="dash" element={<DashLayout />} >
+
+					<Route index element={<Welcome />} />
+
+					<Route path="notes">  {/* dentro do path DASH */}
+						<Route index element={<NotesList />} />
+					</Route>
+					<Route path="users">  {/* dentro do path DASH */}
+						<Route index element={<UsersList />} />
+					</Route>
+
+				</Route> {/* End DASH */}
+
+
 			</Route>
 		</Routes>
 	);
 }
 
 export default App;
+// https://youtu.be/CvCiNeLnZ00?t=9607
