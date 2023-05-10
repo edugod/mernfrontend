a5f6a605009491c6b24bac25e36481d7029a5fd2
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-05-09 21:01:27 -0300

    XDDD

--- src/components/DashFooter.js
+++ src/components/DashFooter.js
@@ -1,34 +1,34 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate, useLoca, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHouse } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const DashFooter = () => {
-	const navigate = useNavigate();
-	const { pathName } = useLocation();
 
-	const onGoHomeClicked = () => navigate("/dash");
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
 
-	let goHomeButton = null;
-	if (pathname !== "/dash") {  //isos só vai aparecer se a gente NÃO está no path "/dash"
-		goHomeButton = (
-			<button
-				className="dash-footer__button icon-button"
-				title="Home"
-				onClick={onGoHomeClicked}
-			>
-				<FontAwesomeIcon icon={faHouse} />
-			</button>
-		);
-	}
+    const onGoHomeClicked = () => navigate('/dash')
 
-	const content = (
-		<footer className="dash-footer">
+    let goHomeButton = null
+    if (pathname !== '/dash') {
+        goHomeButton = (
+            <button
+                className="dash-footer__button icon-button"
+                title="Home"
+                onClick={onGoHomeClicked}
+            >
+                <FontAwesomeIcon icon={faHouse} />
+            </button>
+        )
+    }
+
+    const content = (
+        <footer className="dash-footer">
             {goHomeButton}
-			<p>Current User:</p>
-			<p>Status:</p>
-		</footer>
-	);
-	return content;
-};
-
-export default DashFooter;
+            <p>Current User:</p>
+            <p>Status:</p>
+        </footer>
+    )
+    return content
+}
+export default DashFooter
